7fb132ac7e0b3e7296f82cb17afabe6e365097cb
alolikaghosh15 <alolikaghosh15@gmail.com>
2026-04-27 15:06:47 +0530

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void bubbleSort(int a[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+        printf("Pass %d: ",i+1);
+        for(int k=0;k<n;k++)
+            printf("%d ",a[k]);
+        printf("\n");
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter size: ");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Element a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    bubbleSort(a,n);
+}
