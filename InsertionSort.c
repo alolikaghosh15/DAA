2d9aedb49a83d5ccdc977afd739b812b20c3ee55
alolikaghosh15 <alolikaghosh15@gmail.com>
2026-04-27 15:20:29 +0530

    Create InsertionSort.c

---
+++ InsertionSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void insertionSort(int a[], int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=a[i];
+        int j=i-1;
+        while(j>=0 && a[j]>key)
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+        printf("Pass %d:",i);
+        for(int k=0;k<n;k++)
+            printf("%d ",a[k]);
+        printf("\n");
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter size of the array:");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Element a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    insertionSort(a,n);
+    printf("Sorted Array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
